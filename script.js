4663a089c2e6f6c7efe9f17ed37dd14d8c560fa2
Calvin Roberts <roberts.calvin85@gmail.com>
2021-11-03 21:32:48 +0000

    Style emoji id in function

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ let stars = document.getElementsByClassName('fas'),
 
 stars[0].onclick = function () {
   stars[0].style.color = '#ffd93b';
+  stars[1].style.color = '#e4e4e4';
+  stars[2].style.color = '#e4e4e4';
+  stars[3].style.color = '#e4e4e4';
+  stars[4].style.color = '#e4e4e4';
+  emoji.style.transform = 'translateX(0)';
 };
